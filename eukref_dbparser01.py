3dbaa8d6b57b79287b6d3512919bd9de4e14be5e
Javier del Campo <fonamental@gmail.com>
2015-08-20 11:25:37 -0700

    Update eukref_dbparser01.py

--- eukref_dbparser01.py
+++ eukref_dbparser01.py
@@ -1,46 +1,53 @@
-"""
-
+'''
 usage
-python annotatetree.py thecamoebids.clustered.fasttree.nex thecamoebids.clustered.fasttree.txt
+python annotateclusters.py thecamoebids.clustered.fasttree.txt thecamoebids.clusters.uc expandedclusters.txt
 
-"""
+'''
+
 
 import os
 import sys
 import glob
 import re
 
-infile = open(sys.argv[1],"r")  ### annotated tree nexus file
+infile = open(sys.argv[1],"r")  ### tab delimited outfile of annotatetree.py
 lines = infile.readlines()
 infile.close()
-outfile = open(sys.argv[2],"w") ### tab delimited outfile
+clusterfile = open(sys.argv[2], "r") ### *.uc file
+clusterlines = clusterfile.readlines()
+clusterfile.close()
+outfile = open(sys.argv[3],"w") ### tab delimited outfile
 
-for line in lines:
-	if re.search('!name=',line):
-		print line
-		annotation = line.split('!name="')[1]
-		annotation = annotation.split('"')[0]			
-		if line.startswith("\t'gi+"):
-			accession = line.split("+")[3]
-			accession = accession.split("+")[0]
-			if accession == "noaccesssion":
-				accession2 = line.split("+")[4]
-				accession2 = accession2.split("'")[0]
-				outfile.write(accession2 + "\t" + annotation + "\n")
-			else:
-				outfile.write(accession + "\t" + annotation + "\n")
-		if line.startswith("\t'gi|"):
-			accession = line.split("|")[3]
-			accession = accession.split("|")[0]
-			if accession == "noaccesssion":
-				accession2 = line.split("|")[4]
-				accession2 = accession2.split("'")[0]
-				outfile.write(accession2 + "\t" + annotation + "\n")
-			else:
-				outfile.write(accession + "\t" + annotation + "\n")
-		else:
-			pass
+
+for line in clusterlines:
+	cluster = line.split("\t")[9]
+	cluster = cluster.strip()
+	fullname = line.split("\t")[8]
+	
+	if fullname.startswith("gi+"):
+		accession = line.split("+")[3]
+
+	if fullname.startswith("gi|"):
+		accession = line.split("|")[3]
+
 	else:
 		pass
+
+	if cluster == "*":
+		for i in lines:
+			if i.startswith(accession):
+				#print i
+				outfile.write(i)
+			else:
+				pass	
+
+	else:
+		for i in lines:
+			if i.startswith(cluster):
+				annotation = i.split("\t")[1]
+				outfile.write(accession + "\t" + annotation)
+			else:
+				pass
+
 outfile.close()
 sys.exit()
