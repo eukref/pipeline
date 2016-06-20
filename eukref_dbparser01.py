da1110c3600735ab947ac772a810738471844173
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-06-20 11:29:36 +0200

    Apply autopep8

--- eukref_dbparser01.py
+++ eukref_dbparser01.py
@@ -8,37 +8,38 @@ python annotatetree.py thecamoebids.clustered.fasttree.nex thecamoebids.clustere
 import sys
 import re
 
-infile = open(sys.argv[1],"r")  ### annotated tree nexus file
+infile = open(sys.argv[1], "r")  # annotated tree nexus file
 lines = infile.readlines()
 infile.close()
-outfile = open(sys.argv[2],"w") ### tab delimited outfile
+outfile = open(sys.argv[2], "w")  # tab delimited outfile
 
 for line in lines:
-	if re.search('!name=',line):
-		print line
-		annotation = line.split('!name="')[1]
-		annotation = annotation.split('"')[0]			
-		if line.startswith("\t'gi+"):
-			accession = line.split("+")[3]
-			accession = accession.split("+")[0]
-			if accession == "noaccesssion":
-				accession2 = line.split("+")[4]
-				accession2 = accession2.split("'")[0]
-				outfile.write(accession2 + "\t" + annotation + "\n")
-			else:
-				outfile.write(accession + "\t" + annotation + "\n")
-		if line.startswith("\t'gi|"):
-			accession = line.split("|")[3]
-			accession = accession.split("|")[0]
-			if accession == "noaccesssion":
-				accession2 = line.split("|")[4]
-				accession2 = accession2.split("'")[0]
-				outfile.write(accession2 + "\t" + annotation + "\n")
-			else:
-				outfile.write(accession + "\t" + annotation + "\n")
-		else:
-			pass
-	else:
-		pass
+    if re.search('!name=', line):
+        print line
+        annotation = line.split('!name="')[1]
+        annotation = annotation.split('"')[0]
+        if line.startswith("\t'gi+"):
+            accession = line.split("+")[3]
+            accession = accession.split("+")[0]
+            if accession == "noaccesssion":
+                accession2 = line.split("+")[4]
+                accession2 = accession2.split("'")[0]
+                outfile.write(accession2 + "\t" + annotation + "\n")
+            else:
+                outfile.write(accession + "\t" + annotation + "\n")
+        if line.startswith("\t'gi|"):
+            accession = line.split("|")[3]
+            accession = accession.split("|")[0]
+            if accession == "noaccesssion":
+                accession2 = line.split("|")[4]
+                accession2 = accession2.split("'")[0]
+                outfile.write(accession2 + "\t" + annotation + "\n")
+            else:
+                outfile.write(accession + "\t" + annotation + "\n")
+        else:
+            pass
+    else:
+        pass
 outfile.close()
+
 sys.exit()
