9c7b1221755235952b1c78da51181bdaebd2130d
Javier del Campo <fonamental@gmail.com>
2016-08-11 11:36:27 -0700

    Adding eukref_gbmetadata.py
    eukref_gbmetadata.py will substitute eukref_metadata.pl

---
+++ eukref_gbmetadata.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import sys
+from Bio import SeqIO
+from Bio.Blast import NCBIXML
+#Usage: $python eukref_gbmetadata.py outfile.txt infile.gb
+OUT = open(sys.argv[1], 'w')
+OUT.write("Accession\tTaxonomy\tName\tSource\tEnvironment\tHost\tCountry\tPublication\tAuthors\tJournal\n")
+result_handle = open(sys.argv[2])
+gbfiles = SeqIO.parse(result_handle, 'gb')
+for rec in gbfiles:
+    acc = rec.id
+    source = rec.features[0]
+    if 'taxonomy' in rec.annotations:	
+        taxonomy = "_".join(rec.annotations['taxonomy'])
+    if 'clone' in source.qualifiers:
+        clone = source.qualifiers['clone'][0]
+    elif 'organism' in rec.annotations:
+        clone = rec.annotations['organism']
+    else:
+    	clone = "NA"
+    if 'environmental_sample' in source.qualifiers:
+    	environmental_sample = "Environenmental"
+    else:
+        environmental_sample = "Isolate"
+    if 'isolation_source' in source.qualifiers:
+        isolation_source = source.qualifiers['isolation_source'][0]
+    else:
+        isolation_source = "NA"
+    if 'host' in source.qualifiers:
+        host = source.qualifiers['host'][0]
+    else:
+        host = "NA"
+    if 'country' in source.qualifiers:
+        country = source.qualifiers['country'][0]
+    else:
+        country = "NA"
+    if 'references' in rec.annotations:
+		pubref = rec.annotations['references'][0]
+		authors = pubref.authors
+		title = pubref.title
+		journal = pubref.journal
+    else:
+        title = "NA"
+        authors = "NA"
+        journal = "NA"
+    fields = [acc, taxonomy, clone, environmental_sample, isolation_source, host, country, title, authors, journal]
+    OUT.write("\t".join(fields)+ "\n")
+OUT.close()			
