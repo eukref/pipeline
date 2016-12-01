d53b6f2b1a28b57207170440b73b586ec50f974a
Martin <martin.kolisko@botany.ubc.ca>
2016-12-01 07:02:13 -0800

    New Scripts

---
+++ eukref_dbparse02.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+print "\nexpands clusters" 
+print "Contributors: Matthew Brown, Laura Wegener Parfrey, and Evan Morien"
+print "November 2016"
+print "\nrun: python eukref_dbparser02.py -h for help.\n"
+print "\nExample run: python eukref_dbparser02.py -i Metadata_File -r reference_metadata_file -c current_DB.clusters.uc  -o expandedclusters.txt\n"
+
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='expand clusters')
+parser.add_argument(
+    '-i',
+    '--input_metadata',
+    help='metadata',
+    required=True)
+parser.add_argument(
+    '-r',
+    '--input_reference_metadata',
+    help='reference_metadata',
+    required=True)
+parser.add_argument(
+    '-c',
+    '--input_clusterfile',
+    help='cluster file ==> *.uc file',
+    required=True)
+parser.add_argument(
+    '-o',
+    '--output_expandedclusters',
+    help='metadata file expanded to all sequences in reference database with annotations',
+    required=True)
+    
+args = parser.parse_args()
+
+meta_infile = args.input_metadata
+trim_file = args.input_reference_metadata
+out_file = args.output_expandedclusters
+cluster_file = args.input_clusterfile
+    
+infile = open(cluster_file)
+lines = infile.readlines()
+infile.close()
+
+centroid_d = {}
+
+for line in lines:
+	if line.split()[0] == 'S':
+		centroid_d[line.split('\t')[1]] = line.split('\t')[8]
+	else:
+		pass
+print centroid_d
+acc_centroid_d = {}
+for line in lines:
+	print line
+	print centroid_d[line.split('\t')[1]]
+	print acc_centroid_d
+	acc_centroid_d[line.split('\t')[8]] = centroid_d[line.split('\t')[1]]
+
+#print acc_centroid_d	
+#print acc_centroid_d['AF315604']
+
+infile = open(meta_infile)
+lines = infile.readlines()
+infile.close()
+number_col = len(lines[0].split('\t'))
+meta_d = {}
+for i in lines[1:]:
+	meta_d[i.split('\t')[0]] = i.strip()
+	
+infile = open(trim_file)
+lines = infile.readlines()
+infile.close()
+
+trim_d = {}
+
+for line in lines[1:]:
+#	print line
+#	print number_col
+	annotation = '\t'.join( line.split('\t')[number_col:])
+	trim_d[line.split('\t')[0]] = annotation.strip()
+print trim_d
+print meta_d
+print acc_centroid_d
+out = open(out_file, 'w')
+
+for acc in meta_d:
+	print acc
+	try:
+		out.write('%s\t%s\n' % (meta_d[acc], trim_d[acc_centroid_d[acc]]))
+	except KeyError:
+		print 'WARNING accession number %s does no exist in cluster file' % (acc)
+		out.write('%s %s\n' % (meta_d[acc], trim_d[acc]))
+out.close()
+
+	
+ 
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
---
+++ trim_metadata.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import glob
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Trim metadata table down to the taxa in clustered reference tree')
+parser.add_argument(
+    '-i',
+    '--input_fasta',
+    help='Clustered Reference Tree',
+    required=True)
+parser.add_argument(
+    '-c',
+    '--input_metadatafile',
+    help='Metadata File',
+    required=True)
+parser.add_argument(
+    '-o',
+    '--output_trimmed_metadatafile',
+    help='Trimmed Metadata File Output',
+    required=True)
+    
+args = parser.parse_args()
+
+fas_infile = args.input_fasta
+meta_file = args.input_metadatafile
+out_file = args.output_trimmed_metadatafile
+
+infile = open(fas_infile)
+line = infile.read()
+infile.close()
+seqs = line[1:].split('\n>')
+
+centroids_accs = []
+
+for seq in seqs:
+	acc = seq.split()[0]
+	acc = acc.split('_')[0]
+	if acc != 'OUTGROUP':
+		centroids_accs.append(acc)
+ 
+
+infile = open(meta_file)
+lines = infile.readlines()
+infile.close()
+
+out = open(out_file, "w")
+out.write(lines[0])
+
+counter = 0
+
+for line in lines[1:]:
+	if line.split()[0] in centroids_accs:
+		out.write(line)
+		counter = counter + 1
+
+if counter == len(centroids_accs):
+	print 'Metadata file trimmed to %s records' % (len(centroids_accs))
+else:
+	print 'WARNING: There is %s records recovered from Metadata file and %s sequences in fasta file, check that no records are missing' % (len(centroids_accs), counter) 
+ 		
+out.close()
+
